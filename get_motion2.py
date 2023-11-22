f165ea4171ed187ee51b29db9ceff30378f53de1
Wang_zx <wzx178167@gmail.com>
2023-11-22 16:04:09 +0800

    demo

---
+++ get_motion2.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 打开视频文件
+cap = cv2.VideoCapture('video/motion.avi')
+# 获取视频的帧率
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# 定义位移结果的空列表
+x_displacement = []
+y_displacement = []
+
+# 读取第一帧作为原图像
+cap.set(cv2.CAP_PROP_POS_FRAMES,1)
+ret, frame0 = cap.read()
+# ref_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# 框定特征点
+roi1 = cv2.selectROI(frame0)
+x0, y0, w1, h1 = roi1 #记录矩形框位置信息
+frame_roi = frame0[y0:y0 + h1,x0:x0 + w1 ]#将框内所有坐标选为模板
+yf=100 #竖向匹配范围值
+
+
+# 循环读取视频帧
+while cap.isOpened():
+    # 读取一帧
+    ret, frame = cap.read()
+    if not ret:
+        break
+        # if not ret判断ret的值是否为False，即判断是否读取失败。
+        # 如果读取失败，表示视频的所有帧都已经读取完毕或发生了错误，break语句跳出循环
+
+    frame_fanwei = frame[y0 - yf:y0 + h1 + yf, x0:x0 + w1]  # 将框内所有坐标选为模板
+    # cv2.matchTemplate()将frame_roi置于图像frame中匹配，得出每个位置的匹配度
+    result = cv2.matchTemplate(frame_fanwei, frame_roi, cv2.TM_CCORR_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)#找出匹配度最大的框的左上角坐标
+    # 最小匹配度、最大匹配度、最小匹配度坐标、最大匹配度坐标
+    # 对于单通道图像，这些位置是二维坐标(x, y)；对于多通道图像，这些位置是三维坐标(x, y, c)，其中c表示通道索引
+
+    #计算每一帧特征值的相对位移
+    x1=max_loc[0]
+    y1=max_loc[1]
+    dy=y1 - yf
+    #将所得数据放入列表
+    y_displacement.append(dy)
+
+     # 绘制每一帧与特征值匹配度最大矩形框
+    cv2.rectangle(frame_fanwei, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 0), 2)
+    cv2.rectangle(frame, (x0,y0-yf),(x0+w1,y0+h1+yf ), (0, 255, 0), 2)
+    # 在矩形框上显示位移
+    cv2.putText(frame, f"dy: {dy}", (x0,y0-yf-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    #建立窗口并刷新图像
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release() #释放硬件资源防止报错
+cv2.destroyAllWindows()#关闭窗口
+
+# 绘制位移随时间变化的图像
+time = np.arange(len(y_displacement)) / fps
+
+# 对位移时程进行FFT变换
+fft_y = np.fft.fft(y_displacement)
+# 获取频率轴
+freq = np.fft.fftfreq(len(y_displacement), d=1/fps)
+# 获取主要频率成分的索引
+
+# 获取主要频率
+main_freq_idx_y = np.argmax(np.abs(fft_y))
+main_freq_y = freq[main_freq_idx_y]
+print(f"Main frequency in y direction: {main_freq_y} Hz")
+
+# 获取FFT结果的幅值谱
+amplitude_y = np.abs(fft_y)
+
+# 位移时程图像
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(time, y_displacement, label='y displacement', linewidth= 1)
+plt.xlabel('t(s)')
+plt.ylabel('displacement(pixel)')
+plt.legend()
+
+# 绘制结构幅值随频率变化的图像
+plt.subplot(1, 2, 2)
+plt.plot(freq[:len(freq)//2], amplitude_y[:len(freq)//2], label='y Amplitude', linewidth= 1 )
+plt.xlabel('frequency(Hz)')
+plt.ylabel('A(pixel)')   # get Amplitude 获取振幅
+plt.legend()
+
+plt.tight_layout()
+plt.show()
