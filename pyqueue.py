75696f0333d68b9b449f29fd199b37d157ac3474
Wang_zx <wzx178167@gmail.com>
2023-11-23 21:31:29 +0800

    pyqueue

--- pyqueue.py
+++ pyqueue.py
@@ -9,42 +9,47 @@ cap, fps = GetFps(video)
 VideoRoi, VideoTemplate = GetTemplate(cap)
 x0, y0, w1, h1 = VideoRoi
 
-# 生产者线程
-def producer(q):
+
+def tempalte_dy(q):
+    # 匹配获取dy线程
     while cap.isOpened():
         # 读取一帧
         ret, frame = cap.read()
         if not ret:
             break
-        template_area, yf = TemplateArea(VideoRoi, frame, 100)
-        result = cv2.matchTemplate(template_area, VideoTemplate, cv2.TM_CCORR_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        template_area, yf = GetTemplateArea(VideoRoi, frame)
 
-        # 计算每一帧特征值的相对位移
-        y1 = max_loc[1]
-        dy = y1 - yf
-        y_displacement = q.put(dy)
+        x1, y1, dy = Getdy(template_area, VideoTemplate)
+        q.put(dy)
+
+        # 绘制每一帧与特征值匹配度最大矩形框
+        cv2.rectangle(template_area, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x0, y0 - yf), (x0 + w1, y0 + h1 + yf), (0, 255, 0), 2)
+        # 在矩形框上显示位移
+        cv2.putText(frame, f"dy: {dy}", (x0, y0 - yf - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        # 建立窗口并刷新图像
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
-        time.sleep(0.01)
+    cap.release()  # 释放硬件资源防止报错
+    cv2.destroyAllWindows()  # 关闭窗口
 
 
 
-# 消费者线程
-def consumer(q):
+def tempalte_plot(q):
+    # 绘图线程
     while True:
         data = q.get()  # 从队列中获取数据
-        print(f"Consumed {data}")
+        print(f"dy {data}")
         q.task_done()  # 标记任务完成
-        time.sleep(0.02)
+        # time.sleep(0.02)
 
 if __name__ == '__main__':
     q = queue.Queue()  # 创建一个队列对象
 
     # 创建生产者线程和消费者线程
-    producer_thread = threading.Thread(target=producer, args=(q,))
-    consumer_thread = threading.Thread(target=consumer, args=(q,))
+    producer_thread = threading.Thread(target=tempalte_dy, args=(q,))
+    consumer_thread = threading.Thread(target=tempalte_plot, args=(q,))
 
     # 启动线程
     producer_thread.start()
